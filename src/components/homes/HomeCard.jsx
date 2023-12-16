30f50c09a91f1194f96702e13c9e8063dc57c50c
Your Name <haciyevqurban97@gmail.com>
2023-12-16 19:14:27 +0400

    home section completed

--- src/components/homes/HomeCard.jsx
+++ src/components/homes/HomeCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 import { IoPersonOutline } from "react-icons/io5";
 import { MdOutlinePerson } from "react-icons/md";
 
-const HomeCard = ({ item: { id, cover, name, rating, time, desc, starring, genres, tags, video } }) => {
+const HomeCard = ({ item: { id, cover, name, rating, time, desc, starring, janr, tags, video } }) => {
   return (
     <>
       <div className='box'>
@@ -13,24 +13,29 @@ const HomeCard = ({ item: { id, cover, name, rating, time, desc, starring, genre
         <div className='content flex'>
           <div className='details row'>
             <h1>{name}</h1>
-          
+            <div className='rating flex'>
+           
+
+              
+              <label>{time}</label>
+            </div>
             <p>{desc}</p>
             <div className='cast'>
               <h4>
-                <span>Starring </span>
+                <span>Baş rollarda: </span>
                 {starring}
               </h4>
               <h4>
-                <span>Genres </span>
-                {genres}
+                <span>Janrlar: </span>
+                {janr}
               </h4>
               <h4>
-                <span>Tags </span>
+                <span>Məzmun: </span>
                 {tags}
               </h4>
             </div>
             <button className='primary-btn'>
-              <i className='fas fa-play'></i> PLAY NOW
+              <i className='fas fa-play'></i>  <h3>Başlat</h3>
             </button>
           </div>
           <div className='palyButton row'>
@@ -40,7 +45,7 @@ const HomeCard = ({ item: { id, cover, name, rating, time, desc, starring, genre
                   <img src='./images/play-button.png' alt='' />
                   <img src='./images/play.png' className='change' />
                 </div>
-                WATCH TRAILER
+               Traileri izlə
               </button>
             </Link>
           </div>
